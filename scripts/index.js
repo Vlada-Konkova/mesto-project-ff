c0acec91fd821ea987c4400c0486450eaa0d28bf
Vlada Konkova <luittee93@yandex.ru>
2024-11-11 22:34:33 +0300

    Первая отправка работы

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
-
+const placesList = document.querySelector('.places__list');
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard(card) {
+  const cardTemplate = document.querySelector('#card-template');
+  const cardElement = cardTemplate.content.cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = card.name;
+  cardElement.querySelector('.card__title').textContent = card.name;
+  placesList.append(cardElement);
+
+return cardElement;
+}
 
 // @todo: Функция удаления карточки
 
+placesList.addEventListener('click', function (event){
+  const target = evt.target;
+  const card = target.closest('.card');
+  card.remove();
+})
+
 // @todo: Вывести карточки на страницу
+function renderCards() {
+  initialCards.forEach(card => {
+  createCard(card);
+  });
+  }
+
+  renderCards();
